42ea1e54cd17115d9b930daf242bfe530d614bbd
Theglxry <102861719+Theglxry@users.noreply.github.com>
2023-05-21 10:40:31 +0100

    Quick update

--- script.js
+++ script.js
@@ -5,20 +5,31 @@ const submit = document.querySelector(".submit");
 const btnSelected = document.getElementById("#btn");
 const selected = document.querySelector(".selected");
 let selectedRating;
- 
+
+
 
 // FUNCTIONALITY
 submit.addEventListener("click", function (e) {
   e.preventDefault();
 
-  if (!selectedRating) {
-    return alert("Please rate us!");  
-  }
+   // if (!selectedRating) {
+  // return alert('please rate use') //adding return here casues is called a short circuit 
+  // }
+ if (!selectedRating) {
+// SHAKE ANIMATION IF USER DIDNT RATE
+  const divContainer = document.querySelector(".container");
 
-//   if (selectedRating < 4){
-//      alert("what can we do to serve you better ?😭")
-//   }
-  // swaping contents
+  //call shake function .
+  shakeAnimation(divContainer)
+    return alert("Please rate us 😭");
+
+} else if (selectedRating < 3){
+       alert("what can we do to serve you better ?") //if rate is below 3
+    }
+
+
+
+   // switch to thank you contents
   const rating = document.querySelector(".rating-container");
   const thankYouCard = document.querySelector(".thankyou-section");
   const ratingSpan = document.querySelector("#selected-rating");
@@ -30,7 +41,7 @@ submit.addEventListener("click", function (e) {
 });
 
 
-
+// FUNCTION TO GET USER INPUT
 btnList.forEach((btns) => {
   btns.addEventListener("click", function (e) {
     const clickedBtn = e.target;
@@ -41,6 +52,13 @@ btnList.forEach((btns) => {
   });
 });
 
- 
 
- 
+
+//SHAKE ANIMATION
+function shakeAnimation(shake) {
+  shake.classList.add("shake-animation");
+  setTimeout(function () {
+    shake.classList.remove("shake.animation");
+  }, 500);
+}
+// SHAKE DIV ANIMATION END
